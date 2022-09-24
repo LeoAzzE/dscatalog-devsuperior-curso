77dc23c6a7c6e0d75d5459b597bc4902230205e6
Borges dev <leosull50@hotmail.com>
2022-09-24 15:14:25 -0300

    database seeding

--- backend/src/main/java/com/study/spring/services/CategoryService.java
+++ backend/src/main/java/com/study/spring/services/CategoryService.java
@@ -2,17 +2,20 @@ package com.study.spring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import com.study.spring.entities.Category;
 import com.study.spring.repositories.CategoryRepository;
+
 @Service
 public class CategoryService {
 	
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 		
